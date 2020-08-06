7693b96a2e7755e5e071b627ab6492b3e5ca7653
Jacob Ferragamo <jacob@codeintheschools.org>
2020-08-06 15:01:10 +0000

    take

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class ball(object):
         """ The constructor of the class """
         pygame.sprite.Sprite.__init__(self)
 
-        ball.image = pygame.image.load("PinHeadLarry.png")
+        ball.image = pygame.image.load("")
         
         self.image = pygame.transform.scale(self.image,(50,50))
 
@@ -20,29 +20,14 @@ class ball(object):
         """ Draw on surface """
         surface.blit(self.image, (self.x, self.y))
 
-    def bounce(self,screen_width,screen_height, collider):
-        self.x += self.x_velocity
-        self.y += self.y_velocity
-
-        #IF ball reaches Top
-        if(self.y <= 0):
-          self.y_velocity = 2 
-        #IF ball reaches Bottom
-        if(self.y >= screen_height-50):
-          self.y_velocity = -2
-        #IF ball reaches Right
-        if(self.x >= screen_width-50 or collider):
-          self.x_velocity = -3
-        #If ball reaches Left
-        if( self.x <= 0):
-          self.x_velocity = 3
+    
 
 class paddle(object):
     def __init__(self):
         """ The constructor of the class """
         pygame.sprite.Sprite.__init__(self)
 
-        paddle.image = pygame.image.load("PinHeadLarry.png")
+        paddle.image = pygame.image.load("")
         
         self.image = pygame.transform.scale(self.image,(20,100))
 
@@ -52,24 +37,7 @@ class paddle(object):
     def draw(self, surface):
         """ Draw on surface """
         surface.blit(self.image, (700, self.y))
-    def move(self):
-      pos = pygame.mouse.get_pos()
-
-      #self.x = pos[0]
-      self.y= pos[1]
-
-    def hit(self, obj): #Returns True of False depending if fully triggered. 
-
-      if(obj.x+25 >= self.x and obj.x+25 <= self.x+20 ):
-
-        if(obj.y+25 >= self.y and obj.y+25 <= self.y+100):
-
-          print("True")
-          return True
-        else:
-          return False
-      else:
-        return False
+   
 
 
 
@@ -78,8 +46,7 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-Patty = ball()
-Paddle = paddle()
+
 clock = pygame.time.Clock()
 
 running = True
@@ -91,10 +58,7 @@ while running:
             running = False
 
     screen.fill((255,255,255))
-    Patty.draw(screen)
-    Paddle.draw(screen)
-    Paddle.move()
-    Patty.bounce(screen_width,screen_height,Paddle.hit(Patty))
+    
     pygame.display.update() # update the screen
 
     clock.tick(60)
